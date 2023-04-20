8e63ea845bf089b3bf35ffa6b89f977059d27e16
Alexandr Vinogradov <izum709@.com>
2023-04-20 14:50:06 +0300

    add Task1

---
+++ Task_1For/Program.cs
@@ -0,0 +1,16 @@
+﻿// Методы While VS For
+
+string MethodWhile(int count, string c)
+{
+    
+    string result = "";
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+        
+    }
+    return result;
+}
+
+string res = MethodWhile(10, " text " );
+Console.WriteLine(res);
---
+++ Task_1WhileVSFor/Program.cs
@@ -0,0 +1,16 @@
+﻿// Методы While VS For
+
+string MethodWhile(int count, string c)
+{
+    int i = 0;
+    string result = "";
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = MethodWhile(10, " text " );
+Console.WriteLine(res);
