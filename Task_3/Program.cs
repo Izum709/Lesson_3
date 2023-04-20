a3b8e72ee71d959bb04b95c1dd82d9927502c68f
Alexandr Vinogradov <izum709@.com>
2023-04-20 16:54:38 +0300

    Add Task 3 редактор текста

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿string text   ="— Я думаю, — сказал князь, улыбаясь, — что,"
+              +"ежели бы вас послали вместо нашего милого Винценгероде,"
+              +"вы бы взяли приступом согласие прусского короля."
+              +"Вы так красноречивы. Вы дадите мне чаю?";
+
+
+string Replase( string text, char oldValue, char newValue)
+{
+    string resuit = "";
+    int length = text.Length;
+    for(int i =0 ; i < length; i++)
+    {
+        if (text[i] == oldValue) resuit = resuit + $"{newValue}";
+        else resuit = resuit + $"{text[i]}";
+    }
+    return resuit;
+}
+
+string newText = Replase(text, 'к', 'К');
+Console.WriteLine(newText);
+    
